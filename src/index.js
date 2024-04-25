18f3008f47dbfa225ce4728e93909083a4264e1b
CleoJayCodes <jarnaeblackett@gmail.com>
2024-04-25 14:44:45 +0100

    Integrated the API for poems

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings:
-      "Your demure expression, the unfailing grace with which you meet the small misfortunes which we meet each day.",
-
+    strings: response.data.answer,
     autoStart: true,
     delay: 2,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "8084ta21d096e5o8c8b5457467b3ffa7";
+  let context =
+    "You are an intelligent poem expert who has knowledge about different types of short poems and writes them in basic HTML seperated witha <br> between each line";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
