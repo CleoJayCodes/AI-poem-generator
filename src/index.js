82469f732e891c63016bd2daeeccbe6b636523a9
CleoJayCodes <jarnaeblackett@gmail.com>
2024-04-25 15:41:26 +0100

    Improved User experience (added loading)

--- src/index.js
+++ src/index.js
@@ -14,10 +14,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "8084ta21d096e5o8c8b5457467b3ffa7";
   let context =
-    "You are an intelligent poem expert who has knowledge about different types of short poems and writes them in basic HTML seperated witha <br> between each line";
+    "You are an intelligent poem expert who has knowledge about different types of short poems and writes them in a poetical form making sure there is no title keeping it concise around one paragraph";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<span class="loader"> Generating a Poem about ${instructionsInput.value}</span>`;
   console.log("Generating poem");
   console.log(`prompt: ${prompt}`);
   console.log(`context: ${context}`);
